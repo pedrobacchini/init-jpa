278423bcca272b0afe35ce12144f8d4307c38145
Pedro Bacchini <pedro.bacchini@cilia.com.br>
2019-11-13 13:22:28 -0300

    Insert Client.

--- src/main/java/com/pedrobacchini/registerclient/Example.java
+++ src/main/java/com/pedrobacchini/registerclient/Example.java
@@ -4,6 +4,7 @@ import com.pedrobacchini.registerclient.model.Client;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Example {
@@ -14,9 +15,15 @@ public class Example {
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Client client = entityManager.find(Client.class, 1);
+//        Client client = entityManager.find(Client.class, 1);
+//
+//        System.out.println(client);
 
-        System.out.println(client);
+        Client clientNew = new Client("Pedro");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(clientNew);
+        entityManager.getTransaction().commit();
 
         entityManager.close();
 
--- src/main/java/com/pedrobacchini/registerclient/model/Client.java
+++ src/main/java/com/pedrobacchini/registerclient/model/Client.java
@@ -1,21 +1,25 @@
 package com.pedrobacchini.registerclient.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Getter
-@Setter
 @ToString
 @EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Client {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
 }
